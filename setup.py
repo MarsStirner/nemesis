c669d561825b33030f4e8903c14cf339e8173f50
Serge Antipov <antipov.serge@gmail.com>
2015-03-26 16:30:57 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='nemesis',
+    version='0.1.0',
+    url='https://github.com/hitsl/nemesis',
+    author='hitsl',
+    description='Base MIS module.',
+    long_description=read('readme.md'),
+    include_package_data=True,
+    packages=['nemesis'],
+    platforms='any',
+    #test_suite='nemesis.tests',
+    install_requires=[
+        'Flask',
+        'SQLAlchemy',
+        'Flask-SQLAlchemy',
+        'pytz',
+        'Flask-BabelEx',
+        'Flask-Cache',
+        'Requests',
+        'spinxit',
+        'Flask-Beaker',
+        'Flask-Login',
+        'Flask-Principal',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
