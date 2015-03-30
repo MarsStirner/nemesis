f66707b5b7c8290f220895e9f472a3472d9325e1
swod <roman.s3000@gmail.com>
2015-03-30 14:51:16 +0300

    Version 0.1.0
    Также добавлена поддержка вывода версий приложения (hippo/risar) и версии общего компонента (nemesis).

--- nemesis/context_processors.py
+++ nemesis/context_processors.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
-from pytz import timezone
 from nemesis.app import app
 from datetime import datetime
 from nemesis.lib.user import UserUtils, UserProfileManager
-from version import version as _version, last_change_date
+from version import version as app_version
+from nemesis_version import version as nemesis_version
 from nemesis.lib.enum import Enum
 
 
 @app.context_processor
 def enum():
     ps_url = app.config['PRINT_SUBSYSTEM_URL'].rstrip('/')
-    change_date = timezone(app.config['TIME_ZONE']).localize(last_change_date)
     return {
         'Enum': Enum,
         'LPU_STYLE': app.config['LPU_STYLE'],
@@ -23,7 +22,7 @@ def enum():
         'print_subsystem_print_template': '%s/print_template' % ps_url,
 
         'copy_year': datetime.today().year,
-        'version': _version,
-        'change_date': change_date,
+        'app_version': app_version,
+        'nemesis_version': nemesis_version
     }
 
