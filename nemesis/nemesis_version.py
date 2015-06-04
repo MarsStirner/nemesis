234b4914cb5d261ab56cd3b5285664dacfb0f832
swod <roman.s3000@gmail.com>
2015-06-04 18:23:11 +0300

    Version 1.1dev1

--- nemesis/nemesis_version.py
+++ nemesis/nemesis_version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.0.19'
-last_change_date = datetime(year=2015, month=5, day=28, hour=15)
+version = u'1.1dev1'
+last_change_date = datetime(year=2015, month=6, day=4, hour=18)
