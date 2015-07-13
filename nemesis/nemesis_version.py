e8135b8ece432712c542c235cb9d89cd16b9f7dd
SAntipov <santipov@korusconsulting.ru>
2015-07-13 12:06:15 +0300

    v1.1dev6

--- nemesis/nemesis_version.py
+++ nemesis/nemesis_version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.1dev5'
-last_change_date = datetime(year=2015, month=6, day=30, hour=17)
+version = u'1.1dev6'
+last_change_date = datetime(year=2015, month=7, day=13, hour=12)
