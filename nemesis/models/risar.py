80a2a909b9afa80e8f6e8679184c478a5af81469
plakrisenko <PLakrisenko@korusconsulting.ru>
2015-08-04 13:21:23 +0300

    RIMIS-378 хранение коэффициентов за года

--- nemesis/models/risar.py
+++ nemesis/models/risar.py
@@ -28,3 +28,33 @@ class rbPerinatalRiskRateMkb(db.Model):
 
     perinatal_risk_rate = db.relationship('rbPerinatalRiskRate')
     mkb = db.relationship('MKB')
+
+
+class rbRateType(db.Model):
+    __tablename__ = u'rbRateType'
+    _table_description = u'типы коэффициентов'
+
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.Unicode(45), index=True, nullable=False)
+    name = db.Column(db.Unicode(45), nullable=False)
+
+    def __json__(self):
+        return {
+            'id': self.id,
+            'code': self.code,
+            'name': self.name
+        }
+
+
+class TerritorialRate(db.Model):
+    __tablename__ = u'TerritorialRate'
+    _table_description = u'значения коэффициентов по территориям'
+
+    id = db.Column(db.Integer, primary_key=True)
+    kladr_code = db.Column(db.Unicode(13))
+    rate_type_id = db.Column(db.Integer, db.ForeignKey('rbRateType.id'))
+    year = db.Column(db.Integer)
+    value = db.Column(db.Float)
+    deleted = db.Column(db.Integer, nullable=False, server_default=u"'0'")
+
+    rate_type = db.relationship('rbRateType')
