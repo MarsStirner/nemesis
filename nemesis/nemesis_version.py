cba5c845cde3ac9780624828db6be6359fba073a
Михаил Мальков <m.malkov@bars-open.ru>
2016-06-22 09:31:11 +0300

    v-1.3.4-20160622.0

--- nemesis/nemesis_version.py
+++ nemesis/nemesis_version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.3.4'
-last_change_date = datetime(year=2016, month=5, day=23, hour=15)
+version = u'1.3.4-20160622.0'
+last_change_date = datetime(year=2016, month=6, day=22, hour=9)
