c8dddcbf9d5d62c38fd8aeb57f9c6814fa3f4236
Михаил Мальков <m.malkov@bars-open.ru>
2016-07-01 15:55:27 +0300

    v 1.3.7 TMIS-1150 - Замена блокировок через EventSource на WebSocket

--- nemesis/nemesis_version.py
+++ nemesis/nemesis_version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.3.6'
-last_change_date = datetime(year=2016, month=6, day=29, hour=19)
+version = u'1.3.7'
+last_change_date = datetime(year=2016, month=7, day=1, hour=16)
