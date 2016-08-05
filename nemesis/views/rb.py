fa12273789acefaa19bac7cda5d5121c9f599167
Михаил Мальков <m.malkov@bars-open.ru>
2016-08-05 15:29:45 +0300

    RIMIS-1245 Исполльзование двухуровневого справочника в Талоне 25

--- nemesis/views/rb.py
+++ nemesis/views/rb.py
@@ -11,6 +11,11 @@ from nemesis.systemwide import cache
 __author__ = 'viruzzz-kun'
 
 
+def _insert_id(item):
+   item['id'] = item.get('_id')
+   return item
+
+
 def api_refbook_int(name, code=None):
     if name is None:
         return []
@@ -48,10 +53,7 @@ def api_refbook_int(name, code=None):
             else:
                 return [safe_dict(rb) for rb in ref_book.query.order_by(_order).all()]
 
-    return [
-        {'id': item['_id'], 'name': item['name'], 'code': item['code']}
-        for item in Vesta.get_rb(name)
-    ]
+    return map(_insert_id, Vesta.get_rb(name))
 
 
 def check_rb_value_exists(rb_name, value_code, field_name=None):
