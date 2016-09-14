5b901b6da7f0de0b58428d2464237d9d8c1f1429
Роман Степанов <r.stepanov@bars-open.ru>
2016-09-14 14:51:03 +0300

    Version 2.4.7

--- nemesis/nemesis_version.py
+++ nemesis/nemesis_version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'rsr2.4.6'
-last_change_date = datetime(year=2016, month=9, day=13, hour=21)
+version = u'rsr2.4.7'
+last_change_date = datetime(year=2016, month=9, day=14, hour=15)
