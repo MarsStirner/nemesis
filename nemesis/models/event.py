9bbf6000e7ba994d2fc5729e82f5707ba8c307cd
Роман Степанов <r.stepanov@bars-open.ru>
2016-10-07 12:33:12 +0300

    TMIS-1325 - Обращения с иф вмп из омс

--- nemesis/models/event.py
+++ nemesis/models/event.py
@@ -6,7 +6,7 @@ import re
 from nemesis.lib.agesex import AgeSex, parseAgeSelector
 from nemesis.lib.const import PAYER_EVENT_CODES, STATIONARY_EVENT_CODES, DIAGNOSTIC_EVENT_CODES, \
     POLICLINIC_EVENT_CODES, PAID_EVENT_CODE, OMS_EVENT_CODE, DMS_EVENT_CODE, BUDGET_EVENT_CODE, DAY_HOSPITAL_CODE, \
-    VMP_EVENT_CODE, ADM_PERM_EVENT_CODES
+    VMP_EVENT_CODE, ADM_PERM_EVENT_CODES, VMP_FROM_OMS_EVENT_CODE
 from nemesis.lib.settings import Settings
 from nemesis.models.client import ClientDocument
 from nemesis.models.diagnosis import EventType_rbDiagnosisType
@@ -186,6 +186,10 @@ class Event(db.Model):
     @property
     def is_vmp(self):
         return self.eventType.finance.code == VMP_EVENT_CODE
+
+    @property
+    def is_vmp_from_oms(self):
+        return self.eventType.finance.code == VMP_FROM_OMS_EVENT_CODE
 
     @property
     def is_budget(self):
