9ad0f4bc52d169954d455077e7a2fd53d9f77433
Роман Степанов <r.stepanov@bars-open.ru>
2016-10-24 12:11:49 +0300

    Version 1.3.18

--- nemesis/nemesis_version.py
+++ nemesis/nemesis_version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.3.17'
-last_change_date = datetime(year=2016, month=10, day=7, hour=15)
+version = u'1.3.18'
+last_change_date = datetime(year=2016, month=10, day=24, hour=12)
