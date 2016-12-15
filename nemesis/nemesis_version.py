6d03106810c5782a3b3eb90fe362895613b6734a
Роман Степанов <r.stepanov@bars-open.ru>
2016-12-15 14:51:56 +0300

    RISAR Version 2.4.24

--- nemesis/nemesis_version.py
+++ nemesis/nemesis_version.py
@@ -1,5 +1,5 @@
 
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'rsr2.4.23'
-last_change_date = datetime(year=2016, month=12, day=9, hour=20)
+version = u'rsr2.4.24'
+last_change_date = datetime(year=2016, month=12, day=15, hour=15)
