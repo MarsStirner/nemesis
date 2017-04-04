c36d4205872f30d696a3acd6eb7d6f12d5df6d80
Aleksandr Krivopustov <a.krivopustov@bars-open.ru>
2017-04-04 14:18:08 +0300

    TMIS-1223 проверка "Действителен до" полиса ДМС

--- nemesis/static/js/directives/client-directives.js
+++ nemesis/static/js/directives/client-directives.js
@@ -101,7 +101,7 @@ angular.module('WebMis20.directives').
          ng-class="{\'has-error\': policyForm.pol_enddate[[idPostfix]].$invalid }">\
         <label for="pol_enddate[[idPostfix]]" class="control-label">Действителен до</label>\
         <wm-date id="pol_enddate[[idPostfix]]" name="pol_enddate[[idPostfix]]"\
-                 ng-model="modelPolicy.end_date" ng-disabled="!edit_mode()">\
+                 ng-model="modelPolicy.end_date" ng-disabled="!edit_mode()" ng-required="pType==1 && policyForm.$dirty">\
         </wm-date>\
     </div>\
 </div>\
