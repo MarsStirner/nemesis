337ff0c2a23f399bbb99a17dbb6afb754bc56ea6
Aleksandr Krivopustov <a.krivopustov@bars-open.ru>
2017-04-24 15:41:39 +0300

    Version 1.3.42

--- nemesis/nemesis_version.py
+++ nemesis/nemesis_version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.3.41'
-last_change_date = datetime(year=2017, month=4, day=20, hour=14)
+version = u'1.3.42'
+last_change_date = datetime(year=2017, month=4, day=24, hour=15)
