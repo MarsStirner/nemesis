9e3fb6d834b7b8d4063aaf15f0063c5e8acfe51b
Oleg Tkachenko <o.tkachenko@bars-open.ru>
2017-05-30 12:27:18 +0200

    Version 1.3.44

--- nemesis/nemesis_version.py
+++ nemesis/nemesis_version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.3.43'
-last_change_date = datetime(year=2017, month=5, day=12, hour=18)
+version = u'1.3.44'
+last_change_date = datetime(year=2017, month=5, day=30, hour=12)
